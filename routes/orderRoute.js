af7a3db7a80722cc15576c08bf2d038d96255393
syedalihussain100 <smuheminali@gmail.com>
2023-03-28 04:43:05 -0700

    changes

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,14 +2,19 @@ const express = require("express");
 const order_Route = express.Router();
 const bodyParser = require("body-parser");
 
-const { orderCreate,orderDetails,ordersAll } = require("../controllers/orderController");
+const {
+  orderCreate,
+  orderDetails,
+  ordersAll,
+} = require("../controllers/orderController");
+const organizationAuth = require("../middleware/organizationAuth");
 
 order_Route.use(express.json());
 order_Route.use(bodyParser.json());
 order_Route.use(bodyParser.urlencoded({ extended: true }));
 
-order_Route.post(`/createOrder`,  orderCreate);
-order_Route.get(`/orderDetails/:id`,  orderDetails);
+order_Route.post(`/createOrder`, organizationAuth, orderCreate);
+order_Route.get(`/orderDetails/:id`, organizationAuth, orderDetails);
 order_Route.get(`/orders`, ordersAll);
 
 module.exports = order_Route;
