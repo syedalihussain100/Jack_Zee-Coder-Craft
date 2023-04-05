7885860797dd26abeb3fd8cd210340eb2d514217
jackDelivery <jackdelivery121@gmail.com>
2023-04-05 02:25:50 -0700

    changes

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.use(`/api`, user_Route);
 app.use(`/api`,order_Route);
 
 
-app.use(express.static(path.join(__dirname,"../frontend/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 
